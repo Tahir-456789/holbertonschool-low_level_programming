4cdfa3267925be6d240838b93c7155ad56e9686c
Tahir1234 <tahir.ibrahimov2012@gmail.com>
2025-02-26 13:42:52 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,12 @@
  * File: main.c
  * Author: Tahir Ibrahimov
  * Email: 10736@holbertonstudents.com
- * Description: This program prints a quote about programming.
+ * Description: This file prints a quote about programming.
  */
-
 #include <stdio.h>
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
