f17e3ffc1ea2a653ca275f2fc43c491f9e086ff6
Tahir1234 <tahir.ibrahimov2012@gmail.com>
2025-02-26 12:57:06 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#!/bin/bash
+#/bin/bash
 #include <stdio.h>
 int main()
 {
-char var1 = "Programming is like building a multilingual puzzle"
+char var1 = "Programming is like building a multilingual puzzle";
 puts(var1);
 return (0);
 }
