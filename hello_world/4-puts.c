b5eba594f60b4228eace34e5d3f9e8966b96eb14
Tahir1234 <tahir.ibrahimov2012@gmail.com>
2025-02-26 13:40:42 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,10 @@
+/**
+ * File: main.c
+ * Author: Tahir Ibrahimov
+ * Email: 10736@holbertonstudents.com
+ * Description: This program prints a quote about programming.
+ */
+
 #include <stdio.h>
 int main(void)
 {
