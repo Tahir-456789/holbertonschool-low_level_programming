3c854c0ecc8b4f6906e19695a9a5d79a1a656213
Tahir1234 <tahir.ibrahimov2012@gmail.com>
2025-02-26 13:30:42 +0000

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,21 @@
 /**
- * Holberton School
- * Written by Tahir Ibrahimov (10736@holbertonstudents.com)
- *[[]]
- *[[]]
- *\  /
- * \/
+ * File: main.c
+ * Author: Tahir Ibrahimov
+ * Email: 10736@holbertonstudents.com
+ * Description: This program prints a quote about programming.
  */
-
-
-
 #include <stdio.h>
+/**
+ * main - Prints a string about programming.
+ * 
+ * Description: This function uses the puts function to print
+ * a string about programming.
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
