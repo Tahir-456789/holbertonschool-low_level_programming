5b124c65c9cfe060e9cbb791137699255f4b3154
Tahir1234 <tahir.ibrahimov2012@gmail.com>
2025-02-28 11:40:39 +0000

    commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 /*
  * File: 9-print_comb.c
  * Auth: Tahir
- */
+*/
 
 #include <stdio.h>
 
@@ -19,7 +19,7 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
+
 		putchar(',');
 		putchar(' ');
 	}
